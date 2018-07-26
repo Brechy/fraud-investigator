ceab67ffb082bb6514a5856f7b64722dca984a20
Corina Andariese <candariese@gmail.com>
2018-07-25 23:00:04 -0700

    Added event name and venue name to cards

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import {Button, Card, Image} from 'semantic-ui-react'
+import {Message} from 'semantic-ui-react'
+
+const LOW_THRESHOLD = 1.0/3.0
+const MED_THRESHOLD = 2.0/3.0
+
+// https://repl.it/repls/MadeupSnowOpposites
 
   const Event = (props) => (
     <Card>
       <Card.Content>
-        <Image floated='right' size='mini' src='/images/avatar/large/steve.jpg' />
-        <Card.Header>Steve Sanders</Card.Header>
-        <Card.Meta>Friends of Elliot</Card.Meta>
+        <Image floated='right' size='mini' src='' />
+        <Card.Header>{`${props.event.event_name}`}</Card.Header>
         <Card.Description>
-          Steve wants to add you to the group <strong>best friends</strong>
+        {`${props.event.venue_name}`}
         </Card.Description>
       </Card.Content>
+      <div>
+        <Message>% goes here</Message>
+      </div>
+      <div>
       <Card.Content extra>
         <div className='ui two buttons'>
           <Button basic color='green'>
-            Approve
-          </Button>
-          <Button basic color='red'>
-            Decline
+            More Information
           </Button>
         </div>
       </Card.Content>
+      </div>
     </Card>
 )
 
