4f7c41ac6b5f96a396babc665709b550ec3f0287
Corina Andariese <candariese@gmail.com>
2018-07-26 13:50:29 -0700

    Added firebase deployment tools

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   }
 
   componentDidMount = async () => {
-    const eventsJSON = await fetch('http://localhost:3000/events')
+    const eventsJSON = await fetch('https://fake-db-21522.firebaseio.com/')
     console.log(eventsJSON, 'This is my array of events')
     let events = await eventsJSON.json()
 
