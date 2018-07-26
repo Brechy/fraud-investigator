e8893fd2111f2d7f738f96306c93e27b764c27ca
Corina Andariese <candariese@gmail.com>
2018-07-25 23:35:14 -0700

    Added images and assigned images to low,med,high threshholds

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import {Button, Card, Image} from 'semantic-ui-react'
 import {Message} from 'semantic-ui-react'
+import highkitty from '../img/highkitty.jpg'
+import mediumkitty from '../img/mediumkitty.jpeg'
+import lowkitty from '../img/lowkitty.jpeg'
 
 const LOW_THRESHOLD = 1.0/3.0
 const MED_THRESHOLD = 2.0/3.0
 
 // https://repl.it/repls/MadeupSnowOpposites
 
-  const Event = (props) => (
-    <Card>
+  const Event = (props) => {
+    let image = highkitty
+
+    if (props.event.probability < LOW_THRESHOLD) {
+      image = lowkitty
+    } else if (props.event.probability < MED_THRESHOLD) {
+      image = mediumkitty
+    }
+
+    return (
+      <Card>
       <Card.Content>
-        <Image floated='right' size='mini' src='' />
-        <Card.Header>{`${props.event.event_name}`}</Card.Header>
+        <Image floated='right' size='mini' src={image} />
+        <Card.Header> {`${props.event.event_name}`}</Card.Header>
         <Card.Description>
         {`${props.event.venue_name}`}
         </Card.Description>
@@ -29,6 +41,7 @@ const MED_THRESHOLD = 2.0/3.0
       </Card.Content>
       </div>
     </Card>
-)
+  )
+}
 
 export default Event
